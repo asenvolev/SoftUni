5a0e88372e1f45493dce4a6254a7358c66489c30
asenvolev <asen_volev_gh@abv.bg>
2017-08-21 12:06:26 +0300

    Moving object to the correct folder

---
+++ Arrays/RoundingNumbers/RoundingNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace RoundingNumbers
+{
+    internal class RoundingNumbers
+    {
+        private static void Main(string[] args)
+        {
+            double[] arr = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            int[] roundedNums = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                roundedNums[i] = (int)Math.Round(arr[i], MidpointRounding.AwayFromZero);
+                Console.WriteLine($"{arr[i]}=>{roundedNums[i]}");
+            }
+        }
+    }
+}
---
+++ Arrays/SumArrays/SumArrays.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace SumArrays
+{
+    internal class SumArrays
+    {
+        private static void Main(string[] args)
+        {
+            int[] firstArr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] secondArr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var bigArr = Math.Max(firstArr.Length, secondArr.Length);
+            int[] sumArr = new int[bigArr];
+
+            for (int i = 0; i < bigArr; i++)
+            {
+                sumArr[i] = firstArr[i % firstArr.Length] + secondArr[i % secondArr.Length];
+            }
+
+            Console.WriteLine(string.Join(" ", sumArr));
+        }
+    }
+}
