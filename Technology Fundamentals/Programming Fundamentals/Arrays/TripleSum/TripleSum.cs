4327ee5c74dce2048359f8c13444cad6aba28dd1
asenvolev <asen_volev_gh@abv.bg>
2017-08-21 12:39:33 +0300

    Add files via upload

---
+++ Technology Fundamentals/Programming Fundamentals/Arrays/TripleSum/TripleSum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace TripleSum
+{
+    internal class TripleSum
+    {
+        private static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            bool a = true;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    int sum = arr[i] + arr[j];
+                    if (arr.Contains(sum))
+                    {
+                        Console.WriteLine($"{arr[i]} + {arr[j]} == {sum}");
+                        a = false;
+                    }
+                }
+            }
+            if (a)
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
