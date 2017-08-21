0004f1ad7c3f82520c943c0ad075711f5dcc5fa1
asenvolev <asen_volev_gh@abv.bg>
2017-08-21 12:24:30 +0300

    Arrays-Excercises

---
+++ Technology Fundamentals/Programming Fundamentals/Arrays-Excercises/CompareCharArrays/CompareCharArrays.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace CompareCharArrays
+{
+    internal class CompareCharArrays
+    {
+        private static void Main(string[] args)
+        {
+            char[] first = Console.ReadLine().Split(' ').Select(char.Parse).ToArray();
+            char[] second = Console.ReadLine().Split(' ').Select(char.Parse).ToArray();
+            int shorter = Math.Min(first.Length, second.Length);
+            for (int i = 0; i < shorter; i++)
+            {
+                if (first[i] < second[i])
+                {
+                    Console.WriteLine(string.Join("", first));
+                    Console.WriteLine(string.Join("", second));
+                    break;
+                }
+                else if (first[i] > second[i])
+                {
+                    Console.WriteLine(string.Join("", second));
+                    Console.WriteLine(string.Join("", first));
+                    break;
+                }
+                else
+                {
+                    if (i == shorter - 1)
+                    {
+                        if (first.Length <= second.Length)
+                        {
+                            Console.WriteLine(string.Join("", first));
+                            Console.WriteLine(string.Join("", second));
+                        }
+                        else
+                        {
+                            Console.WriteLine(string.Join("", second));
+                            Console.WriteLine(string.Join("", first));
+                        }
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+}
