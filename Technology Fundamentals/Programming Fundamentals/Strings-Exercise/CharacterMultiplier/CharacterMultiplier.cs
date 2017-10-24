b52fb7ebbbd6071ddc124ce62689cc4d2dedfafb
Asen Volev <asen_volev_gh@abv.bg>
2017-10-24 16:19:55 +0300

    Add files via upload

---
+++ Technology Fundamentals/Programming Fundamentals/Strings-Exercise/CharacterMultiplier/CharacterMultiplier.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterMultiplier
+{
+    class CharacterMultiplier
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split();
+            var firstWord = input[0].ToCharArray();
+            var secondWord = input[1].ToCharArray();
+            int minVal = Math.Min(firstWord.Length, secondWord.Length);
+            int totalSum = 0;
+            
+            for (int i = 0; i < minVal; i++)
+            {
+                int firstNum = firstWord[i];
+                int secNum = secondWord[i];
+                totalSum += firstNum * secNum;
+            }
+            
+            if (minVal == firstWord.Length)
+            {
+                
+                for (int i = minVal; i < secondWord.Length ; i++)
+                {
+                    totalSum += secondWord[i];
+                }
+            }
+            else
+            {
+                for (int i = minVal; i < firstWord.Length; i++)
+                {
+                    totalSum += firstWord[i];
+                }
+            }
+            Console.WriteLine(totalSum);
+
+        }
+    }
+}
