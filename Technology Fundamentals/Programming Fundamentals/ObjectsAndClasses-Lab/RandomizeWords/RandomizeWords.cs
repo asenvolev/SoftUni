dfb881258ce41a6cd31987f5f3ec5629985d27bc
Asen Volev <asen_volev_gh@abv.bg>
2017-10-24 15:54:07 +0300

    Add files via upload

---
+++ Technology Fundamentals/Programming Fundamentals/ObjectsAndClasses-Lab/RandomizeWords/RandomizeWords.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomizeWords
+{
+    class RandomizeWords
+    {
+        static void Main(string[] args)
+        {
+            var words = Console.ReadLine().Split().ToArray();
+            Random rand = new Random();
+            for (int i = 0; i < words.Length; i++)
+            {
+                string current = words[i];
+                int random = rand.Next(0,words.Length);
+                var tempword = words[random];
+                words[i] = tempword;
+                words[random] = current;
+            }
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
