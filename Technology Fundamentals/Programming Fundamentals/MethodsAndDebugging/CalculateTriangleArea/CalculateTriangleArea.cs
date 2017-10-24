4c63d93735c30bc5132b946cf5ff2b3bc4b7058c
Asen Volev <asen_volev_gh@abv.bg>
2017-10-24 14:37:15 +0300

    calculating triangle area

---
+++ Technology Fundamentals/Programming Fundamentals/MethodsAndDebugging/CalculateTriangleArea/CalculateTriangleArea.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateTriangleArea
+{
+    class CalculateTriangleArea
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            Console.WriteLine(TriangleArea(width, height)); 
+
+        }
+
+        private static double TriangleArea(double width, double height)
+        {
+            return height * width / 2;
+        }
+    }
+}
