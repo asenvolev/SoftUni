62ceff9f81bf3ab05f4d363d74913147df8c8d94
Asen Volev <asen_volev_gh@abv.bg>
2017-10-24 15:18:07 +0300

    Add files via upload

---
+++ Technology Fundamentals/Programming Fundamentals/MethodsAndDebugging-Exercises/Fibonacci/Fibonacci.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Fibonacci
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var f1 = 1;
+            var f2 = 1;
+            for (int i = 1; i < n; i++)
+            {
+                var next = f1 + f2;
+                f1 = f2;
+                f2 = next;
+            }
+            Console.WriteLine(f2);
+        }
+    }
+}
