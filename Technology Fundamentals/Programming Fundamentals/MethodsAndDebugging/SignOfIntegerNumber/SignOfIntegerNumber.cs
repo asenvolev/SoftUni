506725278c78e26ad9582ced15c9382f96d6e1c7
Asen Volev <asen_volev_gh@abv.bg>
2017-10-24 15:02:51 +0300

    Add files via upload

---
+++ Technology Fundamentals/Programming Fundamentals/MethodsAndDebugging/SignOfIntegerNumber/SignOfIntegerNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignOfIntegerNumber
+{
+    class SignOfIntegerNumber
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            printSign(number);
+        }
+
+        private static void printSign(int number)
+        {
+            if (number>0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else if (number == 0)
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+        }
+    }
+}
