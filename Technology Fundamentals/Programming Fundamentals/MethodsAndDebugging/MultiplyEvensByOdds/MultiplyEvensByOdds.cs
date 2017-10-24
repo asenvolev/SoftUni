d0532ae2949ae997687ebe81067f6ff1650fb328
Asen Volev <asen_volev_gh@abv.bg>
2017-10-24 14:59:08 +0300

    Add files via upload

---
+++ Technology Fundamentals/Programming Fundamentals/MethodsAndDebugging/MultiplyEvensByOdds/MultiplyEvensByOdds.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplyEvensByOdds
+{
+    class MultiplyEvensByOdds
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int evenSum = 0;
+            int oddSum = 0;
+            evenMulOdd(number, evenSum, oddSum);
+            
+        }
+
+        private static void evenMulOdd(int number, int evenSum, int oddSum)
+        {
+            while (number != 0)
+            {
+                int temp = number % 10;
+                if (temp % 2 == 0)
+                {
+                    evenSum += temp;
+                }
+                else
+                {
+                    oddSum += temp;
+                }
+                number /= 10;
+            }
+
+            Console.WriteLine(oddSum * evenSum);
+        }
+    }
+}
