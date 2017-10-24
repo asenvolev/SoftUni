aa22a79948887423d91a2b417bf32e2cc8555896
Asen Volev <asen_volev_gh@abv.bg>
2017-10-24 15:18:42 +0300

    Add files via upload

---
+++ Technology Fundamentals/Programming Fundamentals/MethodsAndDebugging-Exercises/GeometryCalculator/GeometryCalculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryCalculator
+{
+    class GeometryCalculator
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine().ToLower();
+            switch (type)
+            {
+                default:
+                    Console.WriteLine("No such type");
+                    break;
+                case "triangle":
+                    double sideTRI = double.Parse(Console.ReadLine());
+                    double heightTRI = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"{sideTRI*heightTRI/2:f2}");
+                    break;
+                case "rectangle":
+                    double width = double.Parse(Console.ReadLine());
+                    double heightRec = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"{width * heightRec :f2}");
+                    break;
+                case "square":
+                    double side = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"{side * side:f2}");
+                    break;
+                case "circle":
+                    double radius = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"{Math.PI*radius*radius:f2}");
+                    break;
+            }
+        }
+    }
+}
