cc942d4f489cc3a9bd5f4c64c50cd046f58cc768
Asen Volev <asen_volev_gh@abv.bg>
2017-10-24 15:52:06 +0300

    Add files via upload

---
+++ Technology Fundamentals/Programming Fundamentals/ObjectsAndClasses-Lab/DayOfWeek/DayOfWeek.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayOfWeek
+{
+    class DayOfWeek
+    {
+        static void Main(string[] args)
+        {
+            string dateText = Console.ReadLine();
+            DateTime date = DateTime.ParseExact(dateText, "d-M-yyyy", System.Globalization.CultureInfo.InvariantCulture);
+            Console.WriteLine(date.DayOfWeek);
+        }
+    }
+}
