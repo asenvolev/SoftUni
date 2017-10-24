5252881e15013f9b3a6507876af5f330d11dcf2f
Asen Volev <asen_volev_gh@abv.bg>
2017-10-24 15:19:39 +0300

    Add files via upload

---
+++ Technology Fundamentals/Programming Fundamentals/MethodsAndDebugging-Exercises/HelloName/HelloName.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloName
+{
+    class HelloName
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Print(name);
+        }
+        public static void Print(string name)
+        {
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
