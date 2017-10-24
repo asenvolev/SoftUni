469ce877b85c422a5d0f345f29cee681a3f1db38
Asen Volev <asen_volev_gh@abv.bg>
2017-10-24 14:57:13 +0300

    calculating power on a number

---
+++ Technology Fundamentals/Programming Fundamentals/MethodsAndDebugging/MathPower/MathPower.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathPower
+{
+    class MathPower
+    {
+        static void Main(string[] args)
+        {
+            decimal number = decimal.Parse(Console.ReadLine());
+            decimal power = decimal.Parse(Console.ReadLine());
+            Console.WriteLine(calculatePower(number, power));
+        }
+
+        private static decimal calculatePower(decimal number, decimal power)
+        {
+            decimal temporary = number;
+            for (int i = 0; i < power - 1; i++)
+            {
+                number *= temporary;
+            }
+            return number;
+        }
+    }
+}
