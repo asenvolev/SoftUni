c54fee217d15971843c2d4d3b8ebd6c6e03641d9
Asen Volev <asen_volev_gh@abv.bg>
2017-10-24 16:15:36 +0300

    Add files via upload

---
+++ Technology Fundamentals/Programming Fundamentals/Strings/CountSubstringOccurrences/CountSubstringOccurrences.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountSubstringOccurrences
+{
+    class CountSubstringOccurrences
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().ToLower();
+            var wordToCount = Console.ReadLine().ToLower();
+            int index = 0;
+            int counter = 0;
+            while (true)
+            {
+                var word = input.IndexOf(wordToCount, index);
+                if (word>=0)
+                {
+                    counter++;
+                    index=word+1;
+                    word = 0;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
