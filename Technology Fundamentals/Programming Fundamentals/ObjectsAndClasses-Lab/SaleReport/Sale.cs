b3c9d6e074aaac206f34e7aeac69cc046273e6cb
Asen Volev <asen_volev_gh@abv.bg>
2017-10-24 15:59:31 +0300

    Add files via upload

---
+++ Technology Fundamentals/Programming Fundamentals/ObjectsAndClasses-Lab/SaleReport/Sale.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaleReport
+{
+    class Sale
+    {
+        public string Town { get; set; }
+        public string Product { get; set; }
+        public decimal Price { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal Total => Price * Quantity;
+    }
+}
---
+++ Technology Fundamentals/Programming Fundamentals/ObjectsAndClasses-Lab/SaleReport/SaleReport.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaleReport
+{
+    class SaleReport
+    {
+        static void Main(string[] args)
+        {
+            int numInputs = int.Parse(Console.ReadLine());
+            var Sales = new SortedDictionary<string, decimal>();
+            for (int i = 0; i < numInputs; i++)
+            {
+                var input = Console.ReadLine().Split();
+                var currSale = new Sale
+                {
+                    Town = input[0],
+                    Product = input[1],
+                    Price = decimal.Parse(input[2]),
+                    Quantity = decimal.Parse(input[3])
+                };
+                if (!Sales.ContainsKey(currSale.Town))
+                {
+                    Sales[currSale.Town] = 0;
+                }
+                Sales[currSale.Town] += currSale.Total;
+            }
+            foreach (var item in Sales)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value:f2}");
+            }
+        }
+    }
+}
