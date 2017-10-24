c3175ed19254cf8674dc003993d87f2c00b28489
Asen Volev <asen_volev_gh@abv.bg>
2017-10-24 15:17:20 +0300

    Add files via upload

---
+++ Technology Fundamentals/Programming Fundamentals/MethodsAndDebugging-Exercises/FactorialTrailingZeroes/FactorialTrailingZeroes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialTrailingZeroes
+{
+    class FactorialTrailingZeroes
+    {
+        static void Main(string[] args)
+        {
+            BigInteger number = BigInteger.Parse(Console.ReadLine());
+            BigInteger fact = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                fact *= i;
+            }
+            int count = 0;
+            int check = 0;
+            while (check ==0)
+            {
+                check = (int)(fact % 10);
+                fact /= 10;
+                count++;
+            }
+            Console.WriteLine(count--);
+        }
+    }
+}
