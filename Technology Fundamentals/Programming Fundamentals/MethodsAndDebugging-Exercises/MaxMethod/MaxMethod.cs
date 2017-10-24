e4172b4446244d4cc497156750f0133052f9eb81
Asen Volev <asen_volev_gh@abv.bg>
2017-10-24 15:39:37 +0300

    Add files via upload

---
+++ Technology Fundamentals/Programming Fundamentals/MethodsAndDebugging-Exercises/MaxMethod/MaxMethod.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxMethod
+{
+    class MaxMethod
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+            int numberThree = int.Parse(Console.ReadLine());
+            int temporary = GetMax(numberOne, numberTwo);
+            Console.WriteLine(GetMax(temporary, numberThree));
+
+        }
+        public static int GetMax(int a, int b)
+        {
+            if (a>b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+    }
+}
