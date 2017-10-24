a0625dcf75a5f1457c3bbe14410d766191790197
Asen Volev <asen_volev_gh@abv.bg>
2017-10-24 15:45:30 +0300

    Add files via upload

---
+++ Technology Fundamentals/Programming Fundamentals/ObjectsAndClasses-Lab/BigFactorial/BigFactorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigFactorial
+{
+    class BigFactorial
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            BigInteger result = 1;
+            for (int i = 1; i <= input; i++)
+            {
+                result *= i;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
