25e0dc26305d14062528159c05f97f7df3e4d7ee
Asen Volev <asen_volev_gh@abv.bg>
2017-10-24 15:16:45 +0300

    Add files via upload

---
+++ Technology Fundamentals/Programming Fundamentals/MethodsAndDebugging-Exercises/Factorial/Factorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Factorial
+    {
+        static void Main(string[] args)
+        {
+            BigInteger number = BigInteger.Parse(Console.ReadLine());
+            BigInteger fact = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                fact *= i;
+            }
+            Console.WriteLine(fact);
+        }
+    }
+}
