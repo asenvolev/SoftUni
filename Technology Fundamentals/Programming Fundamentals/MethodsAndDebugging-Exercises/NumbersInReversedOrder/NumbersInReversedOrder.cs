049b92f12b57c214990f37ead06069c92465f2e1
Asen Volev <asen_volev_gh@abv.bg>
2017-10-24 15:42:05 +0300

    Add files via upload

---
+++ Technology Fundamentals/Programming Fundamentals/MethodsAndDebugging-Exercises/NumbersInReversedOrder/NumbersInReversedOrder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersInReversedOrder
+{
+    class NumbersInReversedOrder
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+
+            string reversedNum = num.ToString();
+
+            string output = "";
+            for (int i = reversedNum.Length - 1; i >= 0; i--)
+            {
+                output += reversedNum[i];
+            }
+
+            Console.WriteLine(output);
+        }
+
+
+    }
+}
