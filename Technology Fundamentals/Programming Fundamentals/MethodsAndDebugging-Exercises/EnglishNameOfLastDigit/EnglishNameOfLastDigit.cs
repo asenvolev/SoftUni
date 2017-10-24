b0d79684aee5c799cde2a05c1ad95d307926d62a
Asen Volev <asen_volev_gh@abv.bg>
2017-10-24 15:16:03 +0300

    Add files via upload

---
+++ Technology Fundamentals/Programming Fundamentals/MethodsAndDebugging-Exercises/EnglishNameOfLastDigit/EnglishNameOfLastDigit.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishNameOfLastDigit
+{
+    class EnglishNameOfLastDigit
+    {
+        static void Main(string[] args)
+        {
+            long number = long.Parse(Console.ReadLine());
+            number = number % 10;
+
+            LastNumToString(Math.Abs(number));
+
+        }
+        public static void LastNumToString (long a)
+        {
+            
+            switch (a)
+            {
+                default:
+                    Console.WriteLine("invalid input");
+                    break;
+                case 0:
+                    Console.WriteLine("zero");
+                    break;
+                case 1:
+                    Console.WriteLine("one");
+                    break;
+                case 2:
+                    Console.WriteLine("two");
+                    break;
+                case 3:
+                    Console.WriteLine("three");
+                    break;
+                case 4:
+                    Console.WriteLine("four");
+                    break;
+                case 5:
+                    Console.WriteLine("five");
+                    break;
+                case 6:
+                    Console.WriteLine("six");
+                    break;
+                case 7:
+                    Console.WriteLine("seven");
+                    break;
+                case 8:
+                    Console.WriteLine("eight");
+                    break;
+                case 9:
+                    Console.WriteLine("nine");
+                    break;
+            }
+        }
+    }
+}
