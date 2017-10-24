a441654658132faf6c5c9bac823d082e9d46f45e
Asen Volev <asen_volev_gh@abv.bg>
2017-10-24 14:31:34 +0300

    Methods and debugging - blank receipt
    homework

---
+++ Technology Fundamentals/Programming Fundamentals/MethodsAndDebugging/BlankReceipt/BlankReceipt.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlankReceipt
+{
+    class BlankReceipt
+    {
+        static void Main(string[] args)
+        {
+            printReciept();
+        }
+
+        private static void printReciept()
+        {
+            printRecieptHeader();
+            printRecieptBody();
+            printRecieptFooter();
+        }
+
+        private static void printRecieptFooter()
+        {
+            Console.WriteLine("------------------------------\n\u00A9 SoftUni");
+        }
+
+        private static void printRecieptBody()
+        {
+            Console.WriteLine(@"Charged to____________________
+Received by___________________");
+        }
+
+        private static void printRecieptHeader()
+        {
+            Console.WriteLine("CASH RECEIPT");
+            Console.WriteLine("------------------------------");
+        }
+    }
+}
