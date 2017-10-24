29aee6a674ca518e273703800bdcbee30a98ea35
Asen Volev <asen_volev_gh@abv.bg>
2017-10-24 16:18:18 +0300

    Add files via upload

---
+++ Technology Fundamentals/Programming Fundamentals/Strings/ReverseString/ReverseString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class ReverseString
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            StringBuilder builder = new StringBuilder(input.Length);
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                builder.Append(input[i]);
+            }
+
+            Console.WriteLine(builder.ToString()); 
+        }
+    }
+}
