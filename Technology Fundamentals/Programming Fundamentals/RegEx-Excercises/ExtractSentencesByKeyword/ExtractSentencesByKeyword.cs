749aa43fe826665c285c8f87c294df7af4d04ce7
Asen Volev <asen_volev_gh@abv.bg>
2017-10-24 16:14:41 +0300

    Add files via upload

---
+++ Technology Fundamentals/Programming Fundamentals/RegEx-Excercises/ExtractSentencesByKeyword/ExtractSentencesByKeyword.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ExtractSentencesByKeyword
+{
+    class ExtractSentencesByKeyword
+    {
+        static void Main(string[] args)
+        {
+            var wordtosearch = Console.ReadLine();
+            var input = Console.ReadLine().Split('.','!','?').Select(x=> x.Trim(' '));
+            string patern = "\\b"+ wordtosearch +"\\b";
+            var regex = new Regex(patern);
+            foreach (var sentence in input)
+            {
+                if (regex.IsMatch(sentence))
+                {
+                    Console.WriteLine(sentence);
+                }
+            }
+        }
+    }
+}
