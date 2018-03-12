2dd8525fd1a17df3e6353e6e08bc50eedd7fd52f
asenvolev <asen_volev_gh@abv.bg>
2018-03-12 20:01:26 +0200

    Hash-Tables - Sets and Dictionaries
    implementation and excercises

---
+++ Data Structures/DictionariesHashTablesAndSetsExcercise/CountSymbols/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static void Main()
+    {
+        string input = Console.ReadLine();
+        var letterCounter = new SortedDictionary<char, int>();
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (!letterCounter.ContainsKey(input[i]))
+            {
+                letterCounter.Add(input[i], 0);
+            }
+            letterCounter[input[i]]++;
+        }
+
+        foreach (var pair in letterCounter)
+        {
+            Console.WriteLine($"{pair.Key}: {pair.Value} time/s");
+        }
+    }
+}
+
---
+++ Data Structures/DictionariesHashTablesAndSetsExcercise/Phonebook/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static void Main()
+    {
+        var input = Console.ReadLine().Split(new[] { '-', ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+        var dict = new Dictionary<string, string>();
+        while (input[0] != "search")
+        {
+            if (dict.ContainsKey(input[0]))
+            {
+                dict[input[0]] = input[1];
+            }
+            else
+            {
+                dict.Add(input[0], input[1]);
+            }
+            input = Console.ReadLine().Split(new[] { '-' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+        }
+        input = Console.ReadLine().Split(new[] { '-' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+        while (input[0] != "end")
+        {
+            if (dict.ContainsKey(input[0]))
+            {
+                Console.WriteLine($"{input[0]} -> {dict[input[0]]}");
+            }
+            else
+            {
+                Console.WriteLine($"Contact {input[0]} does not exist.");
+            }
+            input = Console.ReadLine().Split(new[] { '-' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+        }
+    }
+}
+
